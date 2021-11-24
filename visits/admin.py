872067dfd9889c183c40a63234a102a50d2fcb2f
Petar Marić <petar.maric@gmail.com>
2021-11-24 08:20:46 +0100

    Customized admin for  the `visits.Visit` Django model

--- visits/admin.py
+++ visits/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Visit)
+@admin.register(models.Visit)
+class VisitAdmin(admin.ModelAdmin):
+    date_hierarchy = 'visit_time'
+    list_display = [
+        'user_ip',
+        'user_browser',
+        'visit_time',
+    ]
+    list_filter = [
+        'user_ip',
+        'user_browser',
+        'visit_time',
+    ]
+    search_fields = ['user_browser']
